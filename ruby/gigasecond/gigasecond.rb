513d1d117049b6cb625725eaf0e477f9eddb1c33
recroot89 <recroot89@gmail.com>
2019-10-26 02:51:59 +0500

    add gigasecond exercise

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Gigasecond' exercise in this file. Make the tests in
+# `gigasecond_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/gigasecond` directory.
+
+class Gigasecond
+  def self.from(time)
+    time + 10**9
+  end
+end
