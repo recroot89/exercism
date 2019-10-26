eb47d5667cf2acfd9a64497fc0970bb763aea8fd
recroot89 <recroot89@gmail.com>
2019-10-27 01:27:41 +0500

    update two-fer exercise

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+module BookKeeping
+  VERSION = 1
+end
+
 class TwoFer
   def self.two_fer(name = 'you')
     "One for #{name}, one for me."
--- ruby/two-fer/two_fer_test.rb
+++ ruby/two-fer/two_fer_test.rb
@@ -1,21 +1,20 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative 'two_fer'
 
 # Common test data version: 1.1.0 c080bdf
 class TwoFerTest < Minitest::Test
   def test_no_name_given
-    # skip
-    assert_equal "One for you, one for me.", TwoFer.two_fer
+    assert_equal 'One for you, one for me.', TwoFer.two_fer
   end
 
   def test_a_name_given
-    skip
-    assert_equal "One for Alice, one for me.", TwoFer.two_fer("Alice")
+    assert_equal 'One for Alice, one for me.', TwoFer.two_fer('Alice')
   end
 
   def test_another_name_given
-    skip
-    assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
+    assert_equal 'One for Bob, one for me.', TwoFer.two_fer('Bob')
   end
 
   # Problems in exercism evolve over time, as we find better ways to ask
@@ -36,7 +35,6 @@ class TwoFerTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
