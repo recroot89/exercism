87f7fae893aa9ad9b2ca39d0569fcd9f54cd8bf7
recroot89 <recroot89@gmail.com>
2019-11-01 14:20:27 +0500

    add palindrome-products exercise

---
+++ ruby/palindrome-products/palindrome_products.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Palindrome Products' exercise in this file. Make the tests in
+# `palindrome_products_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/palindrome-products` directory.
+
+class Palindromes
+  attr_reader :min_factor, :max_factor
+  attr_accessor :palindromes, :value, :factors
+
+  def initialize(params)
+    @max_factor = params[:max_factor]
+    @min_factor = params[:min_factor] || 1
+  end
+
+  def generate
+    @palindromes = generate_products.filter { |num,| num == num.reverse }
+  end
+
+  def largest
+    @value = palindromes.keys.map(&:to_i).max
+    @factors = palindromes[value.to_s]
+    self
+  end
+
+  def smallest
+    @value = palindromes.keys.map(&:to_i).min
+    @factors = palindromes[value.to_s]
+    self
+  end
+
+  private
+
+  def generate_products
+    iter = lambda do |acc, current_factor|
+      return acc if current_factor > max_factor
+
+      (min_factor..max_factor).each do |num|
+        product_value = (num * current_factor).to_s
+        new_factor_pair = [current_factor, num]
+
+        if acc.key?(product_value)
+          product_value_factors = acc[product_value]
+          reversed_factor_pair = [num, current_factor]
+          unless product_value_factors.include?(reversed_factor_pair)
+            product_value_factors.append(new_factor_pair)
+          end
+        else
+          acc.merge!(product_value => [new_factor_pair])
+        end
+      end
+
+      iter.call(acc, current_factor + 1)
+    end
+
+    iter.call({}, min_factor)
+  end
+end
