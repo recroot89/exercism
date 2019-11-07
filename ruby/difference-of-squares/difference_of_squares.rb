595e002268d9b7a9dad4ea9bd5bc6c6e924d5ff6
recroot89 <recroot89@gmail.com>
2019-11-07 16:34:09 +0500

    add difference-of-squares exercise

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
+# `difference_of_squares_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/difference-of-squares` directory.
+
+class Squares
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    (1..number).sum**2
+  end
+
+  def sum_of_squares
+    (1..number).reduce(0) { |acc, num| acc + num**2 }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
