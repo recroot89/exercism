172e9ac08dee09cb8f108d4ab350278c210f9cf1
recroot89 <recroot89@gmail.com>
2019-11-10 20:33:47 +0500

    add transpose to exercise

---
+++ ruby/transpose/transpose.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Transpose' exercise in this file. Make the tests in
+# `transpose_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/transpose` directory.
+
+class Transpose
+  class << self
+    def transpose(input)
+      return '' if input.empty?
+
+      matrix = input.split(/\n/).map(&:chars)
+      max_index_rows = matrix.each.max_by(&:size).size - 1
+      square_matrix = matrix.reduce([]) { |acc, m| acc.append(m.dup.fill(nil, m.size..max_index_rows)) }
+
+      (0..max_index_rows).reduce([]) do |acc, index|
+        row = square_matrix.map { |sm_row| sm_row[index] }
+        acc.append(row.include?(nil) ? replace_nil_in_row(row).join : row.join)
+      end.join("\n")
+    end
+
+    private
+
+    def replace_nil_in_row(row)
+      row.reverse
+         .drop_while(&:nil?)
+         .reverse
+         .map { |char| char.nil? ? ' ' : char }
+    end
+  end
+end
