1bab5b72b97aacde0ce5926cb46132ad15f3b4f0
recroot89 <recroot89@gmail.com>
2019-11-23 00:51:20 +0500

    add bob exercise

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Bob' exercise in this file. Make the tests in
+# `bob_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/bob` directory.
+
+class Bob
+  class << self
+    def hey(message)
+      remark = message.strip
+      if ilegible?(remark)
+        'Fine. Be that way!'
+      elsif yelling?(remark)
+        question?(remark) ? "Calm down, I know what I'm doing!" : 'Whoa, chill out!'
+      else
+        question?(remark) ? 'Sure.' : 'Whatever.'
+      end
+    end
+
+    private
+
+    def ilegible?(message)
+      message.gsub(/\W/, '').empty? && !message.end_with?('?')
+    end
+
+    def question?(message)
+      message.end_with?('?')
+    end
+
+    def yelling?(message)
+      message.upcase == message && !message.scan(/[A-Za-z]/).empty?
+    end
+  end
+end
