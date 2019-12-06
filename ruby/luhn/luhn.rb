847c28d85aaf8e3685f5aeb1e2e290c841c6cd60
recroot89 <recroot89@gmail.com>
2019-12-06 21:46:59 +0500

    add luhn exercise

---
+++ ruby/luhn/luhn.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Luhn' exercise in this file. Make the tests in
+# `luhn_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/luhn` directory.
+
+class Luhn
+  class << self
+    def valid?(code)
+      code_without_spaces = code.gsub(/\s+/, '')
+      if code_without_spaces.length <= 1 || code_without_spaces.scan(/\D/).any?
+        return false
+      end
+
+      numbers = code_without_spaces.split('').map(&:to_i)
+      (double_every_second_digit(numbers).sum % 10).zero?
+    end
+
+    private
+
+    def double_every_second_digit(numbers)
+      numbers.reverse.map.with_index do |digit, index|
+        double = digit * 2
+        if index.odd?
+          double >= 10 ? double.to_s.split('').map(&:to_i).sum : double
+        else
+          digit
+        end
+      end
+    end
+  end
+end
