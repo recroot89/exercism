60f2f5f2b1e277a665f9fa88fffa7a011b2eea9d
recroot89 <recroot89@gmail.com>
2020-01-06 00:02:41 +0500

    update ruby resistor-color

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,28 +1,9 @@
 # frozen_string_literal: true
 
-# Write your code for the 'Resistor Color' exercise in this file. Make the tests in
-# `resistor_color_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/resistor-color` directory.
-
 class ResistorColor
-  COLOR_CODES = {
-    black: 0,
-    brown: 1,
-    red: 2,
-    orange: 3,
-    yellow: 4,
-    green: 5,
-    blue: 6,
-    violet: 7,
-    grey: 8,
-    white: 9
-  }.freeze
-
-  COLORS = COLOR_CODES.keys.map(&:to_s).freeze
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.color_code(color)
-    COLOR_CODES[color.to_sym]
+    COLORS.index(color)
   end
 end
