333f0e8df6181e5c79b14fd064955179240bb275
recroot89 <recroot89@gmail.com>
2020-01-06 01:02:19 +0500

    update ruby twelve-days

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-# Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-# `twelve_days_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/twelve-days` directory.
-
 class TwelveDays
-  SERIAL_NUMBERS = %w[zero first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth].freeze
+  ORDINAL_NUMBERS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth].freeze
   ITEMS = [
     'a Partridge',
     'two Turtle Doves',
@@ -24,9 +18,9 @@ class TwelveDays
   ].freeze
 
   def self.song
-    (1..12).each_with_object([]) do |index, acc|
-      items = "#{ITEMS.slice(1..index - 1).reverse.join(', ')}, and #{ITEMS.first}"
-      acc << "On the #{SERIAL_NUMBERS[index]} day of Christmas my true love gave to me: #{index == 1 ? ITEMS.first : items} in a Pear Tree.\n"
+    ORDINAL_NUMBERS.each_with_index.each_with_object([]) do |(number, index), acc|
+      items = "#{ITEMS.slice(1..index).reverse.join(', ')}, and #{ITEMS.first}"
+      acc << "On the #{number} day of Christmas my true love gave to me: #{index.zero? ? ITEMS.first : items} in a Pear Tree.\n"
     end.join("\n")
   end
 end
