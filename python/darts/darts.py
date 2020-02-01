4a336fa231a94f8d585c76e3625f2cd6631d0a7b
recroot89 <recroot89@gmail.com>
2020-02-01 17:06:36 +0500

    add python darts

---
+++ python/darts/darts.py
@@ -0,0 +1,11 @@
+import math
+
+def score(x, y):
+    radius = math.sqrt(x**2 + y**2)
+    if radius <= 1:
+        return 10
+    elif radius <= 5:
+        return 5
+    elif radius <= 10:
+        return 1
+    return 0
